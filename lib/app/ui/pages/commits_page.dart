5603ba3cd70d8b5ac18773ebf0f6c67371cd39a8
Anthony Josué Jara Bustamante <anthony@vlorish.com>
2024-02-02 21:52:21 -0500

    Refactor CommitsPage to use StatefulWidget

--- lib/app/ui/pages/commits_page.dart
+++ lib/app/ui/pages/commits_page.dart
@@ -2,8 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:fulltime_take_home_test/blocs/commits/commits_bloc.dart';
 
-class CommitsPage extends StatelessWidget {
+class CommitsPage extends StatefulWidget {
   const CommitsPage({super.key});
+
+  @override
+  State<CommitsPage> createState() => _CommitsPageState();
+}
+
+class _CommitsPageState extends State<CommitsPage> {
 
   @override
   Widget build(BuildContext context) {
